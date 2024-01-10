dc6ca3f19f68755fe05f7f5f78bbc2f5a2cacfcf
Sonu Abraham <sonu.abraham@aremedia.com.au>
2024-01-10 16:30:17 +1100

    Updated solution stack

--- lib/eb-appln-stack.ts
+++ lib/eb-appln-stack.ts
@@ -96,7 +96,7 @@ export class EBApplnStack extends cdk.Stack {
     const elbEnv = new elasticbeanstalk.CfnEnvironment(this, "Environment", {
       environmentName: props?.envName ?? "MyWebAppEnvironment",
       applicationName: app.applicationName || appName,
-      solutionStackName: "64bit Amazon Linux 2 v5.8.0 running Node.js 18",
+      solutionStackName: "64bit Amazon Linux 2023 v6.0.4 running Node.js 18",
       optionSettings: optionSettingProperties,
       versionLabel: appVersionProps.ref,
     });
