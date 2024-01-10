7daf72aea5273ae142fe8a9a94ef9fe57393d04b
Sonu Abraham <sonu.abraham@aremedia.com.au>
2024-01-10 15:17:24 +1100

    Initial commit

---
+++ test/cdk-pipeline-elastic-beanstalk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPipelineElasticBeanstalk from '../lib/cdk-pipeline-elastic-beanstalk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pipeline-elastic-beanstalk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPipelineElasticBeanstalk.CdkPipelineElasticBeanstalkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
