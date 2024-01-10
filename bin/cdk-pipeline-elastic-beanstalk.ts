bcd351f139a5d31f74a8b8de972d350a5eb72c95
Sonu Abraham <sonu.abraham@aremedia.com.au>
2024-01-10 15:44:07 +1100

    empty pipeline

--- bin/cdk-pipeline-elastic-beanstalk.ts
+++ bin/cdk-pipeline-elastic-beanstalk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkPipelineElasticBeanstalkStack } from '../lib/cdk-pipeline-elastic-beanstalk-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkPipelineStack } from "../lib/cdk-pipeline-stack";
 
 const app = new cdk.App();
-new CdkPipelineElasticBeanstalkStack(app, 'CdkPipelineElasticBeanstalkStack', {
+new CdkPipelineStack(app, "CdkPipelineElasticBeanstalkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new CdkPipelineElasticBeanstalkStack(app, 'CdkPipelineElasticBeanstalkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "021007710463", region: "ap-southeast-2" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
